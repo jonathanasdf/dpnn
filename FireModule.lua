d927272133c1427c912c5c905e6e0b813b7cded5
jonathanasdf <jonathanasdf@hotmail.com>
2016-09-16 17:11:45 -0400

    update FireModule to take bn as a parameter

--- FireModule.lua
+++ FireModule.lua
@@ -5,30 +5,35 @@
 
 local FireModule, Parent = torch.class('nn.FireModule', 'nn.Decorator')
 
-function FireModule:__init(nInputPlane, s1x1, e1x1, e3x3, activation)
+function FireModule:__init(nInputPlane, s1x1, e1x1, e3x3, activation, bn)
    self.nInputPlane = nInputPlane
    self.s1x1 = s1x1
    self.e1x1 = e1x1
    self.e3x3 = e3x3
    self.activation = activation or 'ReLU'
+   self.bn = bn or true
 
    if self.s1x1 > (self.e1x1 + self.e3x3) then
       print('Warning: <FireModule> s1x1 is recommended to be smaller'..
             ' then e1x1+e3x3')
    end
-   
+
    self.module = nn.Sequential()
    self.squeeze = nn.SpatialConvolution(nInputPlane, s1x1, 1, 1)
    self.expand = nn.Concat(2)
    self.expand:add(nn.SpatialConvolution(s1x1, e1x1, 1, 1))
    self.expand:add(nn.SpatialConvolution(s1x1, e3x3, 3, 3, 1, 1, 1, 1))
 
+   local activation_args = self.activation == 'ReLU' and true or nil
+
    -- Fire Module
    self.module:add(self.squeeze)
-   self.module:add(nn[self.activation]())
+   if self.bn then self.module:add(nn.SpatialBatchNormalization(s1x1)) end
+   self.module:add(nn[self.activation](activation_args))
    self.module:add(self.expand)
-   self.module:add(nn[self.activation]())
-   
+   if self.bn then self.module:add(nn.SpatialBatchNormalization(e1x1 + e3x3)) end
+   self.module:add(nn[self.activation](activation_args))
+
    Parent.__init(self, self.module)
 end
 
@@ -40,8 +45,7 @@ end
 --]]
 
 function FireModule:__tostring__()
-   return string.format('%s inputPlanes: %d -> Squeeze Planes: %d -> '..
-                        'Expand: %d(1x1) + %d(3x3), activation: %s',
+   return string.format('%s(%d ->  %d -> %d(1x1) + %d(3x3), activation: %s, bn: %s)',
                         torch.type(self), self.nInputPlane, self.s1x1,
-                        self.e1x1, self.e3x3, self.activation)
+                        self.e1x1, self.e3x3, self.activation, tostring(self.bn))
 end
